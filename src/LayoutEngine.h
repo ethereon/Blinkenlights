d7434cf881d58a73717330beca6907cc847854d6
Saumitro Dasgupta <ethereon@gmail.com>
2010-04-12 04:39:43 -0400

    Capstone Freeze

--- src/LayoutEngine.h
+++ src/LayoutEngine.h
@@ -1,10 +1,14 @@
 /*
- *  LayoutEngine.h
- *  Blinkenlights
- *
- *  Created by Saumitro Dasgupta on 4/2/10.
- *  Copyright 2010 __MyCompanyName__. All rights reserved.
- *
+ ================================================================
+ 
+ Layout Engine Interface
+ Blinkenlights / The Myelin Project
+ 
+ Copyright (C) 2010 Saumitro Dasgupta.
+ This code is made available under the MIT License.
+ <http://www.opensource.org/licenses/mit-license.html>
+ 
+ ================================================================
  */
 
 #ifndef __LAYOUT_ENGINE_H__
